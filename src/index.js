edf4eee5523d0cb40fbdce74fc06f4a9e4a2955e
Emil Persson <emil.persson@creuna.se>
2017-02-18 20:53:01 +0100

    Move Node function to own file

--- src/index.js
+++ src/index.js
@@ -1,51 +1,12 @@
-import { html } from 'snabbdom-jsx'
-import isArray from './isArray'
-
 export Engine from './Engine'
+export Node from './Node'
+
 import MutatingDecorator from './MutatingDecorator'
+import Node from './Node'
 
 export const mutating = MutatingDecorator.bind(null, false)
 mutating.sync = MutatingDecorator.bind(null, true)
 mutating.async = mutating
-
-export function Node (tagName, attributes, ...children) {
-  if (
-    attributes != null &&
-    typeof attributes['class'] === 'string'
-  ) {
-    attributes.className = attributes['class']
-    delete attributes['class']
-  }
-  return html(tagName, attributes || undefined, children.filter(invalid).map(normalize))
-}
-
-function invalid (renderable) {
-  if (renderable == null) {
-    return false
-  }
-
-  if (renderable === '') {
-    return false
-  }
-
-  return true
-}
-
-function normalize (renderable) {
-  if (isArray(renderable)) {
-    return renderable.map(normalize)
-  }
-
-  if (typeof renderable !== 'object') {
-    return String(renderable)
-  }
-
-  if (typeof renderable.render === 'function') {
-    return renderable.render()
-  }
-
-  return renderable
-}
 
 // This part makes JSX work even if someone is using a transpiler
 // that converts to `React.createElement` instead of `Node`.
